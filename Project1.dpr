65eb654350436cda4ab16330238a28b318a22e66
sinichkina31diana <47798882+sinichkina31diana@users.noreply.github.com>
2019-02-20 12:21:11 +0300

    Add files via upload

---
+++ Project1.dpr
@@ -0,0 +1,15 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+writeln('hello,world from Delphi!');
+writeln('this is the console application');
+writeln;
+writeln('press <enter> to quit.');
+readln;
+
+end.
